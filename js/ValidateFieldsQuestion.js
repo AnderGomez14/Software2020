17a35d8a1d7aecd904d22f77b7a7d620c3af4774
AnderGomez14 <anderelmejor10@gmail.com>
2020-10-13 19:04:26 +0200

    Coloreh

--- js/ValidateFieldsQuestion.js
+++ js/ValidateFieldsQuestion.js
@@ -1,4 +1,5 @@
-function validar(){
+function validar() {
+    limpia();
     var mail = $('#mail').val();
     var enunciado = $('#enunciado').val();
     var correcta = $('#correcta').val();
@@ -7,31 +8,85 @@ function validar(){
     var inco3 = $('#inco3').val();
     var complejidad = $('#complejidad').val();
     var tema = $('#tema').val();
-    if(mail && enunciado && correcta && inco1 && inco2 && inco3 && complejidad && tema)
-    {
-        if(verificarMail(mail)){
+    if (mail && enunciado && correcta && inco1 && inco2 && inco3 && complejidad && tema) {
+        limpia(mail, enunciado, correcta, inco1, inco2, inco3, tema)
+        if (verificarMail(mail)) {
             comp = parseInt(complejidad);
-            if(1 <=comp && comp <= 3){
-                if(enunciado.length>=10){
+            if (1 <= comp && comp <= 3) {
+                if (enunciado.length >= 10) {
                     $('form').submit();
-                }else{
-                    console.log("Pregunta inferior a 10 caracteres");
+                } else {
+                    $('#resul').text("Pregunta inferior a 10 caracteres");
+                    var aux = $('#lenunciado');
+                    aux.css("color", "red");
                 }
-            }else{
-                console.log("Complejidad incorrecta");
+            } else {
+                $('#resul').text("Complejidad incorrecta");
             }
-        }else{
-            console.log("Correo Incorrecto");
+        } else {
+            $('#resul').text("Correo Incorrecto");
+            var aux = $('#lmail');
+            aux.css("color", "red");
         }
     } else {
-        console.log("Faltan datos");
+        $('#resul').text("Faltan datos");
+        vacia(mail, enunciado, correcta, inco1, inco2, inco3, tema);
+
     }
 }
 
-function verificarMail(Mail)
-{
+function verificarMail(Mail) {
     var regexAlumno = /[a-zA-Z]+[0-9]{3}(@ikasle.ehu.)((eus)|(es))/;
     var regexProf1 = /[a-zA-Z]*(.)[a-zA-Z]*(@ehu.)((eus)|(es))/;
     var regexProf2 = /[a-zA-Z]*(@ehu.)((eus)|(es))/;
     return regexAlumno.test(Mail) || regexProf1.test(Mail) || regexProf2.test(Mail);
+}
+
+function limpia() {
+
+    var aux = $('#lmail');
+    aux.css("color", "black");
+    aux = $('#lenunciado');
+    aux.css("color", "black");
+    aux = $('#lcorrecta');
+    aux.css("color", "black");
+    aux = $('#linco1');
+    aux.css("color", "black");
+    aux = $('#linco2');
+    aux.css("color", "black");
+    aux = $('#linco3');
+    aux.css("color", "black");
+    aux = $('#ltema')
+    aux.css("color", "black");
+}
+
+function vacia(mail, enunciado, correcta, inco1, inco2, inco3, tema) {
+    if (!mail) {
+        var aux = $('#lmail');
+        aux.css("color", "red");
+    }
+    if (!enunciado) {
+        var aux = $('#lenunciado');
+        aux.css("color", "red");
+    }
+    if (!correcta) {
+        var aux = $('#lcorrecta');
+        aux.css("color", "red");
+    }
+    if (!inco1) {
+        var aux = $('#linco1');
+        aux.css("color", "red");
+    }
+    if (!inco2) {
+        var aux = $('#linco2');
+        aux.css("color", "red");
+    }
+    if (!inco3) {
+        var aux = $('#linco3');
+        aux.css("color", "red");
+    }
+    if (!tema) {
+        var aux = $('#ltema');
+        aux.css("color", "red");
+    }
 }
--- php/QuestionForm.php
+++ php/QuestionForm.php
@@ -10,28 +10,29 @@
   <section class="main" id="s1">
     <div>
 		<form id='fquestion' name='fquestion' action='AddQuestion.php'>
-			<label>Email*: </label>
+			<label id="lmail">Email*: </label>
 			<input type="text" id="mail"><br>
-			<label>Enunciado de la pregunta:* </label>
+			<label id='lenunciado'>Enunciado de la pregunta:* </label>
 			<input type="text" id="enunciado"><br>
-			<label>Respuesta correcta*: </label>
+			<label id='lcorrecta'>Respuesta correcta*: </label>
 			<input type="text" id="correcta"><br>
-			<label>Respuesta incorrecta 1*: </label>
+			<label id='linco1'>Respuesta incorrecta 1*: </label>
 			<input type="text" id="inco1"><br>
-			<label>Respuesta incorrecta 2*: </label>
+			<label id='linco2'>Respuesta incorrecta 2*: </label>
 			<input type="text" id="inco2"><br>
-			<label>Respuesta incorrecta 3*: </label>
+			<label id='linco3'>Respuesta incorrecta 3*: </label>
 			<input type="text" id="inco3"><br>
-			<label>Complejidad*: </label>
+			<label id='lcomplejidad'>Complejidad*: </label>
 			<select id="complejidad">
 					<option value="1">Baja</option>
 					<option value="2">Media</option>
 					<option value="3">Alta</option>
 			</select> <br>
-			<label>Tema*: </label>
+			<label id='ltema'>Tema*: </label>
 			<input type="text" id="tema"><br>
 		</form>
-		<input type="button" value="Enviar pregunta" onclick="validar()"><br>
+		<input type="button" value="Enviar pregunta" onclick="validar()"><br><br>
+		<label id="resul"></label>
     </div>
   </section>
   <?php include '../html/Footer.html' ?>
