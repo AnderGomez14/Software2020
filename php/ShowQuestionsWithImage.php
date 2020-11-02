23b0e25fe3c2991ce3d2ae8538f9266b53f2ab47
AnderGomez14 <anderelmejor10@gmail.com>
2020-11-02 15:21:22 +0100

    cambiado nombre de variables

--- php/ShowQuestionsWithImage.php
+++ php/ShowQuestionsWithImage.php
@@ -16,13 +16,13 @@
         echo ('MAL');
         die('Fallo al conectar a MySQL: ' . mysqli_connect_error());
       }
-      $usuarios = mysqli_query($mysqli, "select * from preguntas_imagen");
+      $preguntas = mysqli_query($mysqli, "select * from preguntas_imagen");
       echo '<table border=1> <tr> <th> Mail </th> <th> Enunciado </th> <th> Respuesta correcta </th> <th> Respuesta Incorrecta 1 </th> <th> Respuesta Incorrecta 2 </th> <th> Respuesta Incorrecta 3 </th> <th> Nivel de complejidad </th> <th> Tema </th> <th> Foto </th> </tr>';
       //Uso la funcion htmlspecialchars para asi evitar posibles ataques XSS. 
       //Dado que sin eso, si por ejemplo pones <script>alert(1)</script> como enunciado de la pregunta,
       //al visualizar la tabla te saldria un aviso dejando en evidencia el XSS
       $dir = "../uploads/";
-      while ($row = mysqli_fetch_array($usuarios)) {
+      while ($row = mysqli_fetch_array($preguntas)) {
         echo '<tr><td>' . htmlspecialchars($row['mail']) . '</td> <td>' . htmlspecialchars($row['enum']) .
           '</td> <td>' . htmlspecialchars($row['correcta']) . '</td> <td>' . htmlspecialchars($row['inco1']) . '</td> <td>' .
           htmlspecialchars($row['inco2']) . '</td> <td>' . htmlspecialchars($row['inco3']) . '</td> <td>' . htmlspecialchars($row['complejidad']) .
